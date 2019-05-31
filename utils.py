0b0d24cbf178c9fb201edff1d1db3283250af7d6
Kevin Postlethwait <k.postlethwait24@gmail.com>
2019-05-31 13:49:09 -0400

    Auto add ; Only download .whl file if it doesn't exist

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import auditwheel
+import os
 
 TOP_PACKAGES_FILE = "top-pypi.json"
 BASE_URL = "https://pypi.org/pypi/"
@@ -15,13 +16,21 @@ def get_json_url(package_name):
 # py_version: python3.6 = 36, python2.7 = 27, python2 or python3 = py2.py3, python3 = py3
 # os_arch = manylinux1_x86_64, manylinux1_i686, win32, win_amd64, macosx_10_6_intel...
 def download_wheels(projects, release = "latest", py_version = None, os_arch = None):
+    if not os.path.exists("wheels/"):
+        os.mkdir("wheels")
     for p in projects:
         download_wheel(p, release, py_version, os_arch);
 
 def download_wheel(project, release = "latest", py_version = None, os_arch = None):
+    if not os.path.exists("wheels/"):
+        os.mkdir("wheels")
+
     url = get_json_url(project)
     response = SESSION.get(url)
-    response.raise_for_status();
+    
+    if response.status_code != 200:
+        return
+
     data = response.json()
     
     if release == "latest":
@@ -52,9 +61,12 @@ def download_latest_wheel(data, py_version = None, os_arch = None):
         info = download["filename"].split("-")            
         if(download['packagetype'] == 'bdist_wheel' and
            (not py_version or py_version_sat(py_version, download["python_version"])) and
-           (not os_arch or (os_arch + ".whl") == info[4])):
+           (not os_arch or info[4] == "any.whl" or (os_arch + ".whl") == info[4])):
             url = download['url']
             local_filename = "wheels/" + url.split('/')[-1]
+            if os.path.exists(local_filename):
+                continue
+            
             with requests.get(url, stream=True) as req:
                 req.raise_for_status()
                 with open(local_filename, 'wb') as f:
