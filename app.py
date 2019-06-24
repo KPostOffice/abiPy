735d78d6ed60eac334208cbdf8ed133f052bbe61
Kevin Postlethwait <k.postlethwait24@gmail.com>
2019-06-24 14:05:14 -0400

    Commit app

--- app.py
+++ app.py
@@ -15,6 +15,16 @@ req_outside_lib = []
 all_labels = list(data.keys())
 all_vecs = list(data.values())
 
+# create build vector
+with open('build_data.json', 'r') as f:
+    build_data = json.loads(f.read())
+    build_vec = [None] * (build_data['positions'][-1] + build_data['sizes'][-1])
+    for so in build_data['build_data']:
+        obj = build_data['build_data'][so]
+        start = build_data['positions'][obj['pos']]
+        build_vec[start] = so
+        for sym in obj['symbols']:
+            build_vec[start + obj['symbols'][sym]] = f'{so}:{sym}'
 
 for vec in data:
     if data[vec][0] == 1:
@@ -27,6 +37,13 @@ all_norm = StandardScaler().fit_transform(all_vecs)
 pca = PCA(n_components=2)
 
 req_principle = np.asarray(pca.fit_transform(req_norm))
+pca.fit(req_norm)
+print(pca.explained_variance_ratio_)
+components = pca.components_
+named_variance_1 = [x for _,x in sorted(zip(components[0], build_vec))]
+named_variance_2 = [x for _,x in sorted(zip(components[1], build_vec))]
+print(named_variance_1)
+print(named_variance_2)
 all_principle = np.asarray(pca.fit_transform(all_norm))
 
 # principle_req_df = pd.DataFrame(data = req_principle, columns = ['principle 1', 'principle 2', 'principle 3'])
